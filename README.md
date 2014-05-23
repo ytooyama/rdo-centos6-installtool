4cfd590efbe02f197fadc15bece58c42e7017ed6
ytooyama <tooyama@virtualtech.jp>
2014-05-23 19:15:55 +0900

    1st Commit

---
+++ README.md
@@ -0,0 +1,51 @@
+RDO-CentOS6-InstallTool
+==========
+
+###これはなに
+RDOでOpenStack環境を作るスクリプトです。ただ、公式の手順のコマンドを並べただけです｡
+
+###環境について
+以下の環境で実行することを想定しています｡
+
+- CentOS 6.5
+
+###使いかた
+- CentOS 6.5をインストールしてyum updateを実施します。
+- リポジトリーからパッケージをダウンロードします｡
+- シェルスクリプトを実行可能にします｡
+
+```
+	chmod +x centos6-rdo.sh
+```
+
+- シェルスクリプトを実行します｡
+
+```
+	./centos6-rdo.sh
+```
+
+- 質問に答えます 
+
+```
+	Do you want to Copy the sysctl.conf(y/n)?
+	（デフォルトのカーネルパラメータを上書きするか）
+	Do you want to Set SELinux(y/n)?
+	（SELinuxを設定変更するか）
+	Set the Repo(havana/icehouse/skip)?
+	（インストールするOpenStackのバージョンを指定）
+	Do you want to Custom installation of RDO OpenStack(y/n)?
+	（RDO OpenStackをカスタムインストールするか）
+```
+
+- カスタムインストールを実行する場合、アンサーファイル(answer.txt)が作られてスクリプトは終了します｡
+- answer.txtに設定を書き込んだ後、以下のコマンドを実行する必要があります｡
+
+```
+	packstack --answer-file=answer.txt
+```
+
+- 複数台構成にする場合は、カーネルパラメーターの修正やSELinuxの修正、IPアドレスの設定は予め各ノードの設定を行ってからpackstackコマンドを実施してください。
+
+###RDOってなに？
+
+[公式サイト](http://jp-redhat.com/openstack/rdo/)をご覧ください。
---
+++ centos6-rdo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+#RDO Install Assist v.140522-2000
+#
+yum install vim tracerourte system-config-firewall-tui system-config-network-tui
+
+read -p "Do you want to Copy the sysctl.conf(y/n)?"
+[ "$REPLY" == "y" ] && (cp conf/sysctl.conf /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
+[ "$REPLY" == "n" ] && Skipped!
+
+read -p "Do you want to Set SELinux(y/n)?"
+[ "$REPLY" == "y" ] && (vi /etc/selinux/config;setenforce 0)
+[ "$REPLY" == "n" ] && Skipped!
+
+read -p "Set the Repo(havana/icehouse/skip)?"
+[ "$REPLY" == "havana" ] && yum install -y http://rdo.fedorapeople.org/openstack-havana/rdo-release-havana.rpm
+[ "$REPLY" == "icehouse" ] && yum install -y http://rdo.fedorapeople.org/openstack-icehouse/rdo-release-icehouse.rpm
+[ "$REPLY" == "skip" ] && Skipped!
+
+yum -y update
+yum install -y openstack-packstack python-netaddr
+
+read -p "Do you want to Custom installation of RDO OpenStack(y/n)?"
+[ "$REPLY" == "y" ] && (packstack --gen-answer-file=answer.txt;echo "Edit the answer.txt File,After that Run the packstack command.")
+[ "$REPLY" == "n" ] && packstack --allinone
+
