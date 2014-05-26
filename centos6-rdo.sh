49463fe219dc763398eaee5a5827a25e4fafaa0c
ytooyama <tooyama@virtualtech.jp>
2014-05-26 18:53:27 +0900

    echoの付けわすれを修正

--- centos6-rdo.sh
+++ centos6-rdo.sh
@@ -5,16 +5,16 @@
 yum install vim traceroute system-config-firewall-tui system-config-network-tui
 read -p "Do you want to Copy the sysctl.conf(y/n)?"
 [ "$REPLY" == "y" ] && (cp conf/sysctl.conf /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
-[ "$REPLY" == "n" ] && Skipped!
+[ "$REPLY" == "n" ] && echo Skipped!
 
 read -p "Do you want to Set SELinux(y/n)?"
 [ "$REPLY" == "y" ] && (vi /etc/selinux/config;setenforce 0)
-[ "$REPLY" == "n" ] && Skipped!
+[ "$REPLY" == "n" ] && echo Skipped!
 
 read -p "Set the Repo(havana/icehouse/skip)?"
 [ "$REPLY" == "havana" ] && yum install -y http://rdo.fedorapeople.org/openstack-havana/rdo-release-havana.rpm
 [ "$REPLY" == "icehouse" ] && yum install -y http://rdo.fedorapeople.org/openstack-icehouse/rdo-release-icehouse.rpm
-[ "$REPLY" == "skip" ] && Skipped!
+[ "$REPLY" == "skip" ] && echo Skipped!
 
 yum -y update
 yum install -y openstack-packstack python-netaddr
