7ae3011f500a7e7fb4ac02d7c13eab54e6c7eb19
ytooyama <tooyama@virtualtech.jp>
2014-07-07 11:07:36 +0900

    system-configツールを自動インストールしないように修正

--- rdo-assist-srv.sh
+++ rdo-assist-srv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#RDO Install Assist v.140605-1050
+#RDO Install Assist v.140707-1100
 #
 # ディストリビューション名とバージョンを取得する(参考サイト)
 #http://geektrainee.hatenablog.jp/entry/2013/11/27/022633
@@ -9,12 +9,12 @@ tmp=`cat /etc/issue | head -n 1`
 DIST=`echo $tmp | awk '{print $1}'`
 
 if [[ $DIST =~ "CentOS" ]]; then
-  yum install vim traceroute system-config-firewall-tui system-config-network-tui
+  yum install vim traceroute
   read -p "Do you want to Copy the sysctl.conf(y/n)?"
   [ "$REPLY" == "y" ] && (cp conf/sysctl.conf /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
   [ "$REPLY" == "n" ] && echo Skipped!
 elif [[ $DIST =~ "Fedora" ]]; then
-  yum install vim net-tools traceroute system-config-network-tui
+  yum install vim net-tools traceroute
   read -p "Do you want to Copy the sysctl.conf(y/n)?"
   [ "$REPLY" == "y" ] && (cat conf/source.txt >> /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
   [ "$REPLY" == "n" ] && echo Skipped!
