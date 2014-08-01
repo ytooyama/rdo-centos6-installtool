662ad68e63386371f5eec48015f8b303db59c943
Youhei Tooyama <tooyama@virtualtech.jp>
2014-08-01 18:26:31 +0900

    vimを削除

--- rdo-assist-cli.sh
+++ rdo-assist-cli.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#RDO Install Assist v.140801-1800
+#RDO Install Assist v.140801-1825
 #
 # ディストリビューション名とバージョンを取得する(参考サイト)
 #http://geektrainee.hatenablog.jp/entry/2013/11/27/022633
@@ -11,21 +11,21 @@ DIST=`echo $tmp`
 case $DIST in
 "CentOS release 6.4 (Final)"|"CentOS release 6.5 (Final)")
   echo "You use the CentOS 6.4 or 6.5."
-  yum install vim traceroute
+  yum install traceroute
   read -p "Do you want to Copy the sysctl.conf(y/n)?"
     [ "$REPLY" == "y" ] && (cp conf/sysctl.conf /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
     [ "$REPLY" == "n" ] && echo Skipped!
 ;;
 "Scientific Linux release 6.4 (Carbon)"|"Scientific Linux release 6.5 (Carbon)")
   echo "You use the Scientific 6.4 or 6.5."
-  yum install vim traceroute
+  yum install traceroute
   read -p "Do you want to Copy the sysctl.conf(y/n)?"
     [ "$REPLY" == "y" ] && (cp conf/sysctl.conf /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
     [ "$REPLY" == "n" ] && echo Skipped!
 ;;
 "Fedora release 20 (Heisenbug)")
   echo "You use the Fedora 20."
-  yum install vim net-tools traceroute
+  yum install net-tools traceroute
   read -p "Do you want to Copy the sysctl.conf(y/n)?"
     [ "$REPLY" == "y" ] && (cat conf/source.txt >> /etc/sysctl.conf;sysctl -e -p /etc/sysctl.conf)
     [ "$REPLY" == "n" ] && echo Skipped!
